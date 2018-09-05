2d92bf9a297b583bbd6acf849e02c2024c586a97
nithinn <nithin.nn@gmail.com>
2018-09-05 23:07:09 +0530

    clang-format applied

--- src/timer.cpp
+++ src/timer.cpp
@@ -12,7 +12,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */  
+ */
 
 #include <sys/time.h>
 #include <time.h>
@@ -31,147 +31,144 @@ static Time_t s_clockTick = 0;
  */
 Time_t getMilliSeconds()
 {
-   struct timespec sysTime;
-   static Time_t startTime = 0;
+    struct timespec sysTime;
+    static Time_t   startTime = 0;
 
-   clock_gettime(CLOCK_MONOTONIC_COARSE, &sysTime);
-   Time_t usec = (Time_t)sysTime.tv_sec * 1000000LL + sysTime.tv_nsec/ 1000LL;
-   Time_t msec = usec / 1000;
+    clock_gettime(CLOCK_MONOTONIC_COARSE, &sysTime);
+    Time_t usec = (Time_t)sysTime.tv_sec * 1000000LL + sysTime.tv_nsec / 1000LL;
+    Time_t msec = usec / 1000;
 
-   if (startTime == 0)
-   {
-      startTime = msec - 1;
-   }
+    if (startTime == 0)
+    {
+        startTime = msec - 1;
+    }
 
-   msec = msec - startTime;
-   s_clockTick = msec;
-   return msec;
+    msec        = msec - startTime;
+    s_clockTick = msec;
+    return msec;
 }
 
 VOID getTimeStr(S8 *pStr)
 {
-   LOG_ENTERFN();
+    LOG_ENTERFN();
 
-   struct timeval ptime;
-   struct tm* ptm;
+    struct timeval ptime;
+    struct tm *    ptm;
 
-   gettimeofday(&ptime, NULL);
-   ptm = localtime (&ptime.tv_sec);
+    gettimeofday(&ptime, NULL);
+    ptm = localtime(&ptime.tv_sec);
 
-   /* Format the date and time, year, hour, minute and seconds */
-   strftime (pStr, GSIM_TIME_STR_MAX_LEN, "%a %b %d %Y %H:%M:%S", ptm);
-   LOG_EXITVOID();
+    /* Format the date and time, year, hour, minute and seconds */
+    strftime(pStr, GSIM_TIME_STR_MAX_LEN, "%a %b %d %Y %H:%M:%S", ptm);
+    LOG_EXITVOID();
 }
 
 TaskList *TimeWheel::getPausedTaskList(Time_t time)
 {
-   U32 firstWheelSlot = time % TW_ONE_SLOTS;
-   U32 secondWheelSlot = (time / TW_ONE_SLOTS) % TW_TWO_SLOTS;
-   U32 thirdWheelSlot = (time / (TW_ONE_SLOTS * TW_TWO_SLOTS));
+    U32 firstWheelSlot  = time % TW_ONE_SLOTS;
+    U32 secondWheelSlot = (time / TW_ONE_SLOTS) % TW_TWO_SLOTS;
+    U32 thirdWheelSlot  = (time / (TW_ONE_SLOTS * TW_TWO_SLOTS));
 
-   if ((time / TW_ONE_SLOTS) == (wheelBase / TW_ONE_SLOTS))
-   {
-      return &wheelOne[firstWheelSlot];
-   }
-   else if ((time / (TW_ONE_SLOTS * TW_TWO_SLOTS)) == \
-         (wheelBase / (TW_ONE_SLOTS * TW_TWO_SLOTS)))
-   {
-      return &wheelTwo[secondWheelSlot];
-   }
-   else if (thirdWheelSlot < TW_THREE_SLOTS)
-   {
-      return &wheelThree[thirdWheelSlot];
-   }
-   else
-   {
-      LOG_ERROR("Attempted to schedule a task too far in the future");
-      return NULL;
-   }
+    if ((time / TW_ONE_SLOTS) == (wheelBase / TW_ONE_SLOTS))
+    {
+        return &wheelOne[firstWheelSlot];
+    }
+    else if ((time / (TW_ONE_SLOTS * TW_TWO_SLOTS)) ==
+             (wheelBase / (TW_ONE_SLOTS * TW_TWO_SLOTS)))
+    {
+        return &wheelTwo[secondWheelSlot];
+    }
+    else if (thirdWheelSlot < TW_THREE_SLOTS)
+    {
+        return &wheelThree[thirdWheelSlot];
+    }
+    else
+    {
+        LOG_ERROR("Attempted to schedule a task too far in the future");
+        return NULL;
+    }
 }
 
 /**
- * @brief 
+ * @brief
  *    Iterate through our sorted set of paused tasks, removing those that
- *    should no longer be paused, and adding them to the run queue. 
+ *    should no longer be paused, and adding them to the run queue.
  *
- * @return 
+ * @return
  */
 S32 TimeWheel::resumePausedTasks()
 {
-   int found = 0;
+    int found = 0;
 
-   /* This while loop counts up from the wheelBase (i.e. the time
-    * this function last ran) to the current scheduler time (i.e. clock_tick).
-    */
-   while (wheelBase < s_clockTick)
-   {
-      int slot1 = wheelBase % TW_ONE_SLOTS;
+    /* This while loop counts up from the wheelBase (i.e. the time
+     * this function last ran) to the current scheduler time (i.e. clock_tick).
+     */
+    while (wheelBase < s_clockTick)
+    {
+        int slot1 = wheelBase % TW_ONE_SLOTS;
 
-      /* If slot1 is 0 (i.e. wheelBase is a multiple of 4096ms),
-       * we need to repopulate the first timer wheel with the
-       * contents of the first available slot of the second wheel. */
-      if (slot1 == 0)
-      {
-         /* slot2 represents the slot in the second timer wheel
-          * containing the tasks for the next ~4s. So when
-          * wheelBase is 4096, wheel2[1] will be moved into wheel 1,
-          * when wheelBase of 8192 wheel2[2] will be moved into
-          * wheel 1, etc. */
-         int slot2 = (wheelBase / TW_ONE_SLOTS) % TW_TWO_SLOTS;
+        /* If slot1 is 0 (i.e. wheelBase is a multiple of 4096ms),
+         * we need to repopulate the first timer wheel with the
+         * contents of the first available slot of the second wheel. */
+        if (slot1 == 0)
+        {
+            /* slot2 represents the slot in the second timer wheel
+             * containing the tasks for the next ~4s. So when
+             * wheelBase is 4096, wheel2[1] will be moved into wheel 1,
+             * when wheelBase of 8192 wheel2[2] will be moved into
+             * wheel 1, etc. */
+            int slot2 = (wheelBase / TW_ONE_SLOTS) % TW_TWO_SLOTS;
 
-         /* If slot2 is also zero, we must migrate tasks from slot3 into 
-          * slot2.
-          */
-         if (slot2 == 0)
-         {
-            /* Same logic above, except that each slot of wheel3
-               contains the next 69 minutes of tasks, enough to
-               completely fill wheel 2. */
-            int slot3 = ((wheelBase / TW_ONE_SLOTS) / TW_TWO_SLOTS);
-            for (TaskListItr l3it = wheelThree[slot3].begin();
-                  l3it != wheelThree[slot3].end();
-                  l3it++)
+            /* If slot2 is also zero, we must migrate tasks from slot3 into
+             * slot2.
+             */
+            if (slot2 == 0)
             {
-               /* Migrate this task to wheel two. */
-               (*l3it)->recalcWheel();
+                /* Same logic above, except that each slot of wheel3
+                   contains the next 69 minutes of tasks, enough to
+                   completely fill wheel 2. */
+                int slot3 = ((wheelBase / TW_ONE_SLOTS) / TW_TWO_SLOTS);
+                for (TaskListItr l3it = wheelThree[slot3].begin();
+                     l3it != wheelThree[slot3].end(); l3it++)
+                {
+                    /* Migrate this task to wheel two. */
+                    (*l3it)->recalcWheel();
+                }
+
+                wheelThree[slot3].clear();
             }
 
-            wheelThree[slot3].clear();
-         }
+            /* Repopulate wheel 1 from wheel 2 (which will now be full
+             * of the tasks pulled from wheel 3, if that was
+             * necessary)
+             */
+            for (TaskListItr l2it = wheelTwo[slot2].begin();
+                 l2it != wheelTwo[slot2].end(); l2it++)
+            {
+                /* Migrate this task to wheel one. */
+                (*l2it)->recalcWheel();
+            }
 
-         /* Repopulate wheel 1 from wheel 2 (which will now be full
-          * of the tasks pulled from wheel 3, if that was
-          * necessary)
-          */
-         for (TaskListItr l2it = wheelTwo[slot2].begin();
-               l2it != wheelTwo[slot2].end();
-               l2it++)
-         {
-            /* Migrate this task to wheel one. */
-            (*l2it)->recalcWheel();
-         }
+            wheelTwo[slot2].clear();
+        }
 
-         wheelTwo[slot2].clear();
-      }
+        /* Move tasks from the current slot of wheel 1 (i.e. the tasks
+         * scheduled to fire in the 1ms interval represented by wheelBase)
+         * onto a run queue.
+         */
+        found += wheelOne[slot1].size();
+        for (TaskListItr it = wheelOne[slot1].begin();
+             it != wheelOne[slot1].end(); it++)
+        {
+            (*it)->setRunning();
+            count--;
+        }
 
-      /* Move tasks from the current slot of wheel 1 (i.e. the tasks
-       * scheduled to fire in the 1ms interval represented by wheelBase)
-       * onto a run queue.
-       */
-      found += wheelOne[slot1].size();
-      for (TaskListItr it = wheelOne[slot1].begin();\
-            it != wheelOne[slot1].end();
-            it++)
-      {
-         (*it)->setRunning();
-         count--;
-      }
+        wheelOne[slot1].clear();
+        wheelBase++;
+    }
 
-      wheelOne[slot1].clear();
-      wheelBase++;
-   }
-
-   return found;
+    return found;
 }
 
 /**
@@ -181,20 +178,20 @@ S32 TimeWheel::resumePausedTasks()
  */
 VOID TimeWheel::addTask(Task *task)
 {
-   Time_t wake = task->wake();
+    Time_t wake = task->wake();
 
-   /* if wakeup time is less than wheelbase add the task into runnable list */
-   if (wake < wheelBase)
-   {
-      task->setRunning();
-      return;
-   }
+    /* if wakeup time is less than wheelbase add the task into runnable list */
+    if (wake < wheelBase)
+    {
+        task->setRunning();
+        return;
+    }
 
-   TaskList *pauseList = getPausedTaskList(wake);
-   TaskListItr itr = pauseList->insert(pauseList->end(), task);
-   task->m_pausedTaskList = pauseList;
-   task->m_pausedTaskItr = itr;
-   count++;
+    TaskList *  pauseList  = getPausedTaskList(wake);
+    TaskListItr itr        = pauseList->insert(pauseList->end(), task);
+    task->m_pausedTaskList = pauseList;
+    task->m_pausedTaskItr  = itr;
+    count++;
 }
 
 /**
@@ -211,7 +208,7 @@ VOID TimeWheel::removeTask(Task *task)
 
 TimeWheel::TimeWheel()
 {
-    count = 0;
+    count     = 0;
     wheelBase = s_clockTick;
 }
 
@@ -219,6 +216,3 @@ Counter TimeWheel::size()
 {
     return count;
 }
-
-
-
