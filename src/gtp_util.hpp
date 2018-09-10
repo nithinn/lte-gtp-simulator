e16ac6a9cad2a3b7f30c944f0200705e151af099
nithinn <nithin.nn@gmail.com>
2018-09-10 14:44:32 +0530

    including header files to make util.hpp compilable

--- src/gtp_util.hpp
+++ src/gtp_util.hpp
@@ -12,31 +12,38 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */  
+ */
 
 // GTP utility functions
 
 #ifndef _GTP_UTIL_HPP_
 #define _GTP_UTIL_HPP_
 
-#define GTP_MSG_NAME_LEN         64
-#define GTP_IE_NAME_LEN          64
-#define GTP_MSG_XML_TAG_MAX_LEN  16
+#include <types.hpp>
+#include <logger.hpp>
+#include <macros.hpp>
+#include <gtp_macro.hpp>
+#include <gtp_types.hpp>
 
-S8* gtpGetMsgName(GtpMsgType_t msgType);
-GtpIeType_t gtpGetIeType(const S8   *pIeName);
-GtpMsgType_t gtpGetMsgType(const S8 *pXmlMsgTag);
+#define GTP_MSG_NAME_LEN 64
+#define GTP_IE_NAME_LEN 64
+#define GTP_MSG_XML_TAG_MAX_LEN 16
+
+S8 *             gtpGetMsgName(GtpMsgType_t msgType);
+GtpIeType_t      gtpGetIeType(const S8 *pIeName);
+GtpMsgType_t     gtpGetMsgType(const S8 *pXmlMsgTag);
 GtpMsgCategory_t gtpGetMsgCategory(GtpMsgType_t msgType);
-GtpLength_t gtpConvStrToHex(const Buffer *pBuf, U8 *pDst);
-GtpLength_t gtpConvStrToHex(const HexString *value, U8 *pDst);
-EXTERN U32 gtpConvStrToU32(const S8 *pVal, U32 len);
+GtpLength_t      gtpConvStrToHex(const Buffer *pBuf, U8 *pDst);
+GtpLength_t      gtpConvStrToHex(const HexString *value, U8 *pDst);
+EXTERN U32  gtpConvStrToU32(const S8 *pVal, U32 len);
 GtpIfType_t gtpConvStrToIfType(const S8 *pVal, U32 len);
-IpAddr convIpStrToIpAddr(const S8* pIp, U32 len);
-VOID decIeHdr(U8 *pBuf, GtpIeHdr *pHdr);
-U32 encodeImsi(S8 *pImsiStr, U32 imsiStrLen, U8 *pBuf);
+IpAddr      convIpStrToIpAddr(const S8 *pIp, U32 len);
+VOID        decIeHdr(U8 *pBuf, GtpIeHdr *pHdr);
+U32         encodeImsi(S8 *pImsiStr, U32 imsiStrLen, U8 *pBuf);
 EXTERN VOID numericStrIncriment(S8 *pStr, U32 len);
-PUBLIC U8* getImsiBufPtr(Buffer *pGtpcBuf);
-EXTERN VOID gtpUtlEncPlmnId(GtpPlmnId_t *pPlmnId, U8* pBuf);
+PUBLIC U8 *getImsiBufPtr(Buffer *pGtpcBuf);
+EXTERN VOID gtpUtlEncPlmnId(GtpPlmnId_t *pPlmnId, U8 *pBuf);
 PUBLIC S8 *gtpGetIeName(GtpIeType_t ieType);
+PUBLIC U8 gtpCharToHex(U8 c);
 
 #endif
